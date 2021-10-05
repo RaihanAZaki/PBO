f2f5bc3a5919febe2ee8a8c5c2f9c5ef268e44d5
RaihanZ <89891935+RaihanAZaki@users.noreply.github.com>
2021-10-05 22:24:10 +0700

    Add files via upload

---
+++ WEEK5/MyClass.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    int x = 5;
+
+    public static void name(String[] args) {
+        MyClass myObj1 = new MyClass();
+        MyClass myObj2 = new MyClass();
+        System.out.println(myObj1.x);
+        System.out.println(myObj1.x);
+    }
+}
